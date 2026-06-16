628212ee93e205d4363259b2f0f69362a8bef6de
Alon A. Rabinowitz <45129051+AlonRR@users.noreply.github.com>
2026-06-16 12:11:43 +0300

    chore(main): disable demo builds and drop unused imports

--- main.py
+++ main.py
@@ -2,21 +2,16 @@ import logging
 from pathlib import Path
 from typing import cast
 
-import build123d as bd
-from build123d import Compound, Cone, Cylinder, Pos, Rot, ShapeList
+from build123d import Compound, Pos, Rot, ShapeList
 
 from print_scripts_tree_d.export import save_stl
 from print_scripts_tree_d.params import (
     CylinderClipParams,
     RoundedBoxParams,
-    TableParams,
 )
 from print_scripts_tree_d.shapes import (
-    make_column,
     make_cylinder_clip,
-    make_hexagonal_mesh,
     make_rounded_box,
-    make_table,
 )
 
 
@@ -32,53 +27,53 @@ def main() -> None:
     models = Path("models")
     models.mkdir(exist_ok=True)
 
-    # Rounded box
-    bp = RoundedBoxParams()
-    box = make_rounded_box(
-        length=bp.length,
-        width=bp.width,
-        height=bp.height,
-        wall_thickness=bp.wall_thickness,
-        corner_radius=bp.corner_radius,
-        top_fillet_radius=0.8,
-        bottom_fillet_radius=0.5,
-    )
-    save_stl(box, models / "rounded_box.stl")
+    # # Rounded box
+    # bp = RoundedBoxParams()
+    # box = make_rounded_box(
+    #     length=bp.length,
+    #     width=bp.width,
+    #     height=bp.height,
+    #     wall_thickness=bp.wall_thickness,
+    #     corner_radius=bp.corner_radius,
+    #     top_fillet_radius=0.8,
+    #     bottom_fillet_radius=0.5,
+    # )
+    # save_stl(box, models / "rounded_box.stl")
 
-    # Table with hex-panel top and columns
-    tp = TableParams()
-    col_r = tp.column.diameter / 2
-    col_body = Cylinder(col_r, tp.column.height)
-    col_foot = Cone(
-        bottom_radius=col_r * 0.67,
-        top_radius=col_r,
-        height=tp.column.diameter,
-    )
-    column = make_column(
-        body=col_body,
-        height=tp.column.height,
-        foot=col_foot,
-        diameter=tp.column.diameter,
-        gusset_size=tp.column.gusset_size,
-        gusset_thickness=tp.column.gusset_thickness,
-        gusset_position_z=tp.column.gusset_position_z,
-        gusset_orientation_xy=tp.column.gusset_orientation_xy,
-    )
-    hp = tp.top
-    table_top = make_hexagonal_mesh(
-        length=hp.length,
-        width=hp.width,
-        thickness=hp.thickness,
-        hex_radius=hp.hex_radius,
-        spacing=hp.spacing,
-        outer_border=hp.outer_border,
-    )
-    table = make_table(
-        table_top=table_top,
-        columns=[column.rotate(angle=180, axis=bd.Axis.X)],
-        column_positions=tp.column_positions,
-    )
-    save_stl(table, models / "table.stl")
+    # # Table with hex-panel top and columns
+    # tp = TableParams()
+    # col_r = tp.column.diameter / 2
+    # col_body = Cylinder(col_r, tp.column.height)
+    # col_foot = Cone(
+    #     bottom_radius=col_r * 0.67,
+    #     top_radius=col_r,
+    #     height=tp.column.diameter,
+    # )
+    # column = make_column(
+    #     body=col_body,
+    #     height=tp.column.height,
+    #     foot=col_foot,
+    #     diameter=tp.column.diameter,
+    #     gusset_size=tp.column.gusset_size,
+    #     gusset_thickness=tp.column.gusset_thickness,
+    #     gusset_position_z=tp.column.gusset_position_z,
+    #     gusset_orientation_xy=tp.column.gusset_orientation_xy,
+    # )
+    # hp = tp.top
+    # table_top = make_hexagonal_mesh(
+    #     length=hp.length,
+    #     width=hp.width,
+    #     thickness=hp.thickness,
+    #     hex_radius=hp.hex_radius,
+    #     spacing=hp.spacing,
+    #     outer_border=hp.outer_border,
+    # )
+    # table = make_table(
+    #     table_top=table_top,
+    #     columns=[column.rotate(angle=180, axis=bd.Axis.X)],
+    #     column_positions=tp.column_positions,
+    # )
+    # save_stl(table, models / "table.stl")
 
     # Box with cylinder snap clip
     cp = CylinderClipParams()
@@ -112,6 +107,7 @@ def main() -> None:
         tab_width=cp.tab_width,
         slot_width=cp.slot_width,
         clearance=cp.clearance,
+        lead_in_fillet=cp.lead_in_fillet,
         flat_bottom=cp.flat_bottom,
         flat_fillet_r=cp.flat_fillet_r,
         flat_inner_margin=cp.flat_inner_margin,
